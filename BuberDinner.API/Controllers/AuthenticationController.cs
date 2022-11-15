831a565cdbffe73892deada5d1588547ea4090b4
Matheus Barbosa <matheusbarbosasilva.br@gmail.com>
2022-11-15 13:53:42 -0300

    Initial commit

---
+++ BuberDinner.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,59 @@
+using BuberDinner.Application.Services.Authentication;
+using BuberDinner.Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.API.Controllers;
+
+    [ApiController]
+    [Route("auth")]
+    public class AuthenticationController : ControllerBase
+    {
+
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [Route("register")]
+        public IActionResult Register(RegisterRequest request) 
+        {
+            var authResult = _authenticationService.Register(
+                request.FirstName,
+                request.LastName, 
+                request.Email, 
+                request.Password);
+
+            var response = new AuthenticationResponse(
+                authResult.Id,
+                authResult.firstName,
+                authResult.lastName,
+                authResult.Email,
+                authResult.Token
+            );
+
+            return Ok(response);
+        }
+
+        [Route("login")]
+        public IActionResult Login(LoginRequest request)
+        {
+            var authResult = _authenticationService.Login(
+                request.Email, 
+                request.Password);
+
+            var response = new AuthenticationResponse(
+                authResult.Id,
+                authResult.firstName,
+                authResult.lastName,
+                authResult.Email,
+                authResult.Token
+            );
+
+            return Ok(response);
+        }
+    }
+
+
+// Stopped at 31:55
